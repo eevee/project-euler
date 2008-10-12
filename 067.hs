04daff23af588c9e00aa2ffbb67edd03881c8eca
Eevee <ferrox@veekun.com>
2008-10-11 17:50:21 -0700

    Cleaned up problem 67's answer a bit; vars a-e is a bit overboard

--- 067.hs
+++ 067.hs
@@ -121,12 +121,6 @@ triangle = [ [59],
              [23, 33, 44, 81, 80, 92, 93, 75, 94, 88, 23, 61, 39, 76, 22, 03, 28, 94, 32, 06, 49, 65, 41, 34, 18, 23, 08, 47, 62, 60, 03, 63, 33, 13, 80, 52, 31, 54, 73, 43, 70, 26, 16, 69, 57, 87, 83, 31, 03, 93, 70, 81, 47, 95, 77, 44, 29, 68, 39, 51, 56, 59, 63, 07, 25, 70, 07, 77, 43, 53, 64, 03, 94, 42, 95, 39, 18, 01, 66, 21, 16, 97, 20, 50, 90, 16, 70, 10, 95, 69, 29, 06, 25, 61, 41, 26, 15, 59, 63, 35]
              ]
 
--- Simple function for finding the max element in a list
-listMax :: [Integer] -> Integer
-listMax []     = 0
-listMax (a:[]) = a
-listMax (a:b)  = max a (listMax b)
-
 -- Heart of the matter.  This collapses two arrays of integers into one, by
 -- increasing every element of the second by the higher of either the element
 -- in the first with the same index or the one immediately following.  That,
@@ -137,9 +131,12 @@ listMax (a:b)  = max a (listMax b)
 --     10 9 15 9
 -- Note that you have to prepend the implied zero, so the two provided arrays
 -- are the same size.
+-- The ultimate algorithm here is to find the maximum possible value we COULD
+-- have walked so far at each row in the triangle, until we know the maximum
+-- for every endpoint in the last row.  The answer is then the max of those.
 collapseTwoRows :: [Integer] -> [Integer] -> [Integer]
-collapseTwoRows (a:[]) (d:[]) = [a + d]
-collapseTwoRows (a:(b:c)) (d:e) = ((max a b) + d) : (collapseTwoRows (b:c) e)
+collapseTwoRows (a:[]) (b:[]) = [a + b]
+collapseTwoRows (a1:a2:as) (b:bs) = (b + max a1 a2) : (collapseTwoRows (a2:as) bs)
 
 -- Uses collapseTwoRows to collapse the triangle into a single array of the
 -- maximum possible sums reachable at each element on the bottom row.
@@ -147,4 +144,4 @@ collapsedTriangle :: [[Integer]] -> [Integer]
 collapsedTriangle (a:[])  = a
 collapsedTriangle (a:b:c) = collapsedTriangle ((collapseTwoRows (0:a) b) : c)
 
-main = putStrLn ((show . listMax . collapsedTriangle) triangle)
+main = putStrLn ((show . maximum . collapsedTriangle) triangle)
