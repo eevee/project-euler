87033d5a8b729f1affaca6995768a584fd9ac82f
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-10-30 15:58:35 -0400

    Refactor Ruby Problem 1

--- heteroglot/001.rb
+++ heteroglot/001.rb
@@ -8,11 +8,4 @@
 #
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum = 0
-for i in (1 .. 999)
-    if i % 3 == 0 or i % 5 == 0
-        sum += i
-    end
-end
-
-puts sum
+puts (1..999).select { |n| n % 3 == 0 || n % 5 == 0 }.reduce :+
